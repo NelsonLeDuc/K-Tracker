62d78df3b4d45162bc206722c70a4fc114160589
Nelson LeDuc <wolfon33@gmail.com>
2014-12-01 17:58:41 -0600

    Bunch of cleanup, replace app delegate with swift

---
+++ Keto/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  Keto
+//
+//  Created by Nelson LeDuc on 12/1/14.
+//  Copyright (c) 2014 Nelson LeDuc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate : UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+    {
+        createRootViewController()
+        
+        return true
+    }
+    
+    private func createRootViewController()
+    {
+        let containerViewController = KTARootViewController()
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.rootViewController = containerViewController
+        self.window?.makeKeyAndVisible()
+    }
+    
+}
